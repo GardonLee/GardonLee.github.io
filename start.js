2c49e62850ee2804556ae33536144d1cee4bf0bd
GardonLee <15720600373@163.com>
2016-05-14 11:33:04 +0800

    这个勤奋的青年更新了: 2016-05-14 11:32:57

---
+++ start.js
@@ -0,0 +1,6 @@
+var process = require('child_process');
+process.exec(' hexo g -d', function (error, stdout, stderr) {
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+});
